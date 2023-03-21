d6c56e58566ef950dece1dd9e77a1d95de56c21c
avillavera <avillavera@gmail.com>
2023-03-21 01:34:51 -0400

    monogoose db connection setup

---
+++ mern/server/db/shared/mongo-manager.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const URI = process.env.ATLAS_URI;
+
+
+const openMongoConnection = () => {
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function callback () {
+        console.log("connected to MongoDB");
+    });
+    mongoose.connect(URI);
+};
+
+mongoose.set('strictQuery', true)
+
+module.exports = {openMongoConnection};
