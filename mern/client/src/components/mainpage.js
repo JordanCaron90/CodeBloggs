435b09be40bda4c8fccd3bc2379f26e79773c8e5
Manoj Kumar Muthu <manoj.muthu.k@gmail.com>
2023-03-24 22:50:52 -0400

    main page almost done

--- mern/client/src/components/mainpage.js
+++ mern/client/src/components/mainpage.js
@@ -9,6 +9,7 @@ import passwordIcon from "../assets/images/padlock_321783.png"
 
 export default function User() {
   const session_id = Cookies.get('token');
+  const [user_id, setUserId] = useState([]);
   const [records, setRecords] = useState([]);
   const navigate = useNavigate();
 
@@ -16,24 +17,24 @@ export default function User() {
     async function getRecords() {
       try {
         const response = await fetch(`http://localhost:5000/session/${session_id}`);
-        console.log(session_id)
         if (!response.ok) {
           const message = `An error occurred: ${response.statusText}`;
           throw new Error(message);
         }
-      
-        const records = await response.json();
-        setRecords(Array.isArray(records) ? records : []);
+        const responseData = await response.json();
+        console.log(responseData); // add this line to check the response data
+        const user = responseData.data.user;
+        console.log(user); // add this line to check the user data
+        setRecords([user]);
+        
       } catch (error) {
         console.error(error);
         window.alert("Error fetching records");
       }
     }
-    
     getRecords();
   }, [session_id]);
-
-
+  
   return (
     <div className="main">
       <div className="sub-main1">
@@ -42,20 +43,17 @@ export default function User() {
             <img src={profile} alt="profile" className="profile" />
           </div>
           <div className="card-container">
-            <Card style={{ width: '30rem', margin: '1rem', height: '20rem' }}>
-            {records.map((record) => (
-        <Card key={record.id}>
-          <Card.Body>
-            <Card.Title>{record.first_name}</Card.Title>
-            <Card.Text>{record.description}</Card.Text>
-            <button onClick={() => navigate(`/user/${record.id}`)}>View User</button>
-          </Card.Body>
-        </Card>
-      ))}
-            </Card>
+            {records.map((user) => (
+                <Card style={{ width: '30rem', margin: '1rem', height: '20rem' }} key={user._id}>
+                <Card.Body>
+                    <Card.Title>{user.first_name}</Card.Title>
+                    <Card.Text>{user.birthday}</Card.Text>
+                    <button onClick={() => navigate(`/user/${user._id}`)}>View User</button>
+                </Card.Body>
+                </Card>
+                 ))}
           </div>
         </div>
-
        
           <div className="card-container">
             <Card style={{ width: '30rem', margin: '1rem', height: '30rem' }}>
