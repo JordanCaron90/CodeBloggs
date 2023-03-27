7b3ff16720409deab6b1233000f94d434c63266b
avillavera <avillavera@gmail.com>
2023-03-27 16:01:23 -0400

    adjusted app, network, and usercard to proj restructure on client side

---
+++ mern/client/src/components/mainViews/networkViews.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+import { getCookie } from 'react-use-cookie';
+import UserCard from '../modal/UserCard';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function NetworkView() {
+    const [userId, setUserId] = useState();
+    const [users, setUsers] = useState([]);
+    const columnsPerRow = 3;
+
+    useEffect(async() => {
+
+        const getUser = async () => {
+            const token = getCookie('token');
+            
+            try {
+                const fetchResponse = await fetch(`http://localhost:5000/session/${token}`);
+                const data = await fetchResponse.json();
+                return data;
+            } catch (e) {
+                return e;
+            }   
+
+        };
+
+        const response = await getUser();
+        setUserId(response.data.user._id);
+
+    },[]);
+
+    useEffect(async() => {
+
+        const getUsersExceptSelf = async () => {
+            try {
+                const fetchResponse = await fetch(`http://localhost:5000/users/${userId}`);
+                const data = await fetchResponse.json();
+                return data;
+            } catch (e) {
+                return e;
+            }   
+        };
+        if(userId){
+            const response = await getUsersExceptSelf();
+            setUsers(response.data);
+            console.log(response.data)
+        }
+
+    },[userId]);
+
+    function userList() {
+        return users.map((user) => {
+          return (
+            <Col key={user._id}>
+                <UserCard user={user} key={user._id} />
+            </Col>
+          );
+        });
+    }
+
+  return (
+     <div style={{marginLeft: 100}}>
+        <Container fluid>
+            <Row xs={1} md={columnsPerRow}>
+                {userList()}
+            </Row>
+        </Container>
+    </div>
+  )
+}
