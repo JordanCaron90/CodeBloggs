c66128fe9ec94cf0d6059eae86c93f4814fae03f
robensolivier <robens.olivier@gmail.com>
2023-03-29 11:30:22 -0400

    header done

--- mern/client/src/components/navigations/headercomp.js
+++ mern/client/src/components/navigations/headercomp.js
@@ -1,49 +1,80 @@
 import React, { useState } from 'react';
-// import Navbar from './navbar';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import '../css/headercomp.css';
-// import profile from '../../assets/images/CodeBloggsgraphic.png';
-import Modal from '../modal/modal';
-
-import { Navbar, Nav, Dropdown, Button } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
+import { Navbar, Nav, Dropdown, Button, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../../assets/images/CodeBloggsgraphic.png';
 
+
 export default function Header() {
+  const [showModal, setShowModal] = useState(false);
+  const [postTitle, setPostTitle] = useState('');
+  const [postText, setPostText] = useState('');
+
+  const handlePostSubmit = () => {
+    // Implement post submission logic here
+    console.log(`Title: ${postTitle}`);
+    console.log(`Text: ${postText}`);
+    setShowModal(false);
+  };
 
   return (
-    <Navbar bg="light" expand="lg" style={{ borderBottom: "2px solid #8d88ea" }}>
-      <Navbar.Brand href="#home">
-        <img
-          src={logo}
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-          alt="Logo"
-        />
-        Codebloggs
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto"></Nav>
-        <Nav>
-          <Button variant="outline-primary" className="mr-2">
+    <>
+      <Navbar bg="light" expand="lg" style={{ borderBottom: '2px solid #8d88ea' }}>
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="Logo"
+          />
+          Codebloggs
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto"></Nav>
+          <Nav>
+            <Button style={{backgroundColor: "#8d88ea" }}variant="outline-primary" className="mr-2" onClick={() => setShowModal(true)}>
+              Post
+            </Button>
+            <Dropdown>
+              <Dropdown.Toggle variant="outline-primary" id="user-dropdown">
+                User
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#account-settings">Account Settings</Dropdown.Item>
+                <Dropdown.Item href="#logout">Log Out</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Post a new article</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="postTitle">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" placeholder="Enter title" value={postTitle} onChange={(e) => setPostTitle(e.target.value)} />
+            </Form.Group>
+            <Form.Group controlId="postText">
+              <Form.Label>Text</Form.Label>
+              <Form.Control as="textarea" rows={3} placeholder="Enter text" value={postText} onChange={(e) => setPostText(e.target.value)} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handlePostSubmit}>
             Post
           </Button>
-          <Dropdown>
-            <Dropdown.Toggle variant="outline-primary" id="user-dropdown">
-              User
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item href="#account-settings">
-                Account Settings
-              </Dropdown.Item>
-              <Dropdown.Item href="#logout">Log Out</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 // export default function Headercomp() {
